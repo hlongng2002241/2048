8353c0ed401bf54ff803ad8d488f22cdbc968c1b
Nguyen Ngoc Dung <84455292+DungNasSa10@users.noreply.github.com>
2021-12-04 11:03:58 +0700

    Update minimax_alpha_beta.py

--- minimax_alpha_beta.py
+++ minimax_alpha_beta.py
@@ -1,30 +1,27 @@
 from game.grid import Grid
-from sys import maxsize as MAX_INT
 from .algorithm import Algorithm
-from copy import deepcopy
 
 
 class MinimaxAlphaBeta(Algorithm):
 
     def __init__(self, max_depth) -> None:
         super().__init__(max_depth)
-        self.max_depth = max_depth
 
-    def max_move(self, grid: Grid, alpha: int, beta: int, depth: int, max_depth: int):
-        current_score = self.eval.evaluate(grid)
+    def max_move(self, grid: Grid, alpha: int, beta: int, depth: int):
+        current_score = self.eval.evaluate(grid, True)
 
-        if depth > max_depth  or grid.is_terminal(who='max'):
+        if depth > self.max_depth or grid.is_terminal(who='max'):
             return current_score
 
         moves = grid.get_children('max')
-        best_move, max_score = moves[0], -MAX_INT
+        best_move, max_score = moves[0], - self.eval.INFINITY
 
         for move in moves:
-            state = Grid(current_score)
-            state.board = deepcopy(grid.board)
+            state = Grid(None)
+            Grid.copy(grid.board, state.board)
             state.move(move)
             pre_score = max_score
-            max_score = max(max_score, self.min_move(state, alpha, beta, depth + 1, max_depth))
+            max_score = max(max_score, self.exp_move(state, alpha, beta, depth + 1))
 
             if max_score > pre_score and depth == 1:
                 best_move = move
@@ -35,17 +32,16 @@ class MinimaxAlphaBeta(Algorithm):
             return best_move, max_score
         return max_score
 
-    def min_move(self, grid: Grid, alpha: int, beta: int, depth: int, max_depth: int):
-        current_score = self.eval.evaluate(grid)
-
+    def exp_move(self, grid: Grid, alpha: int, beta: int, depth: int):
+        current_score = self.eval.evaluate(grid, True)
         moves = grid.get_children('min')
-        min_score = MAX_INT
+        min_score = self.eval.INFINITY
 
         for move in moves:
-            state = Grid(current_score)
-            state.board = deepcopy(grid.board)
+            state = Grid(None)
+            Grid.copy(grid.board, state.board)
             state.place_tile(move[0], move[1], move[2])
-            min_score = min(min_score, self.max_move(state, alpha, beta, depth + 1, max_depth))
+            min_score = min(min_score, self.max_move(state, alpha, beta, depth + 1))
 
             if min_score <= alpha:
                 return min_score
@@ -56,14 +52,5 @@ class MinimaxAlphaBeta(Algorithm):
         return min_score
 
     def best_move(self, grid: Grid):
-        available_moves = grid.get_available_moves_for_max()
-        best_move = available_moves[0] if available_moves else None
-        max_score = -MAX_INT
-
-        # Iterative deepening
-        for d in range(1, self.max_depth):
-            move, score = self.max_move(grid, -MAX_INT, MAX_INT, 1, d)
-            if score > max_score:
-                max_score, best_move = score, move
-
-        grid.move(best_move)
+        best_move, max_score = self.max_move(grid, - self.eval.INFINITY, self.eval.INFINITY, 1)
+        grid.move(best_move, True)
