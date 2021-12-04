a76e3eab1d95234221b852b37cd930389d0fb409
hlongng2002241 <nguyenhailongng@gmail.com>
2021-12-04 09:21:31 +0700

    Update MinimaxAB and expectmax

--- algorithm/expectmax.py
+++ algorithm/expectmax.py
@@ -1,5 +1,4 @@
 from . algorithm import Algorithm
-from . point import Point
 from game.grid import Grid
 
 
@@ -7,102 +6,93 @@ class Expectmax(Algorithm):
     def __init__(self, max_depth) -> None:
         super().__init__(max_depth=max_depth)
 
-        self.use_sum   = False
-        self.four_dir  = False
+        self.__use_invert_sum   = True
+        # self.__weights          = [0, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+        self.__weights          = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256]
+        # self.__weights          = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-    def max_move(self, grid: Grid, depth) -> Point:
-        current_score = self.eval.evaluate(grid.board, True)
+    def max_move(self, grid: Grid, depth) -> tuple[int, int]:
+        """
 
-        if current_score.points[0] == self.eval.GAME_OVER:
-            return current_score
+        Returns
+        -------
+            tuple of best_move and best_score
+        
+        """
+        current_score       = self.eval.evaluate(grid, True)
+        
+        if depth >= self.max_depth:
+            if self.__use_invert_sum:
+                return -1, current_score * self.__weights[depth]
+            return -1, current_score
 
-        if depth >=self.max_depth:
-            return current_score
+        if current_score == self.eval.GAME_OVER:
+            return -1, current_score * (self.max_depth - depth + 1) 
 
-        moves       = [
-            grid.move_down,
-            grid.move_left,
-            grid.move_right,
-            # grid.move_up,
-        ]
-        if self.four_dir:
-            moves.append(grid.move_up)
+        dirs                = [1, 2, 3]
+        best_move           = -1
+        best_score          = -self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
 
-        INF         = self.eval.INFINITY * (self.max_depth if self.use_sum else 1)
-        best_score  = Point([-INF], self.eval.num_eval)
-
-        for move in moves:
-            save_board = list()
-            grid.copy(grid.board, save_board)
-            if move():
-                score       = self.expecti_move(grid, depth + 1)
+        for direct in dirs:
+            save_board      = list()
+            if grid.can_move(direct):
+                grid.copy(grid.board, save_board)
+                grid.move(direct)
+                _, score    = self.min_move(grid, depth + 1)
                 grid.copy(save_board, grid.board)
+
+                score      += current_score * int(self.__use_invert_sum) * self.__weights[depth]
                 
-                score       = score + current_score * (1 if self.use_sum else 0)
                 if best_score < score:
-                    best_score = score.copy()
+                    best_score  = score
+                    best_move   = direct
 
-        return best_score
+        return best_move, best_score
 
-    def expecti_move(self, grid: Grid, depth) -> Point:
-        current_score = self.eval.evaluate(grid.board, False)
+    def min_move(self, grid: Grid, depth) -> tuple[int, int]:
+        """
+        Returns
+        -------
+            tuple of best_move and best_score
+        """
+        current_score       = self.eval.evaluate(grid, False)
 
-        if current_score.points[0] == self.eval.GAME_OVER:
-            return current_score
+        if depth >= self.max_depth:
+            if self.__use_invert_sum:
+                return -1, current_score * self.__weights[depth]
+            return -1, current_score
+        
+        if current_score == self.eval.GAME_OVER:
+            return -1, current_score * (self.max_depth - depth + 1)
 
-        if depth >=self.max_depth:
-            return current_score
+        ROW, COLUMN         = grid.ROW, grid.COLUMN
+        RATE                = grid.RANDOM_4_RATE
 
-        ROW, COLUMN = grid.ROW, grid.COLUMN
-        RATE        = grid.RANDOM_4_RATE
-
-        best_score  = Point([0 for i in range(self.eval.num_eval)])
-        cnt         = 0
+        best_score          = 0
+        cnt                 = 0
 
         for r in range(ROW):
             for c in range(COLUMN):
                 if grid.board[r][c] == 0:
                     grid.board[r][c]    = 2
-                    score_2             = self.max_move(grid, depth + 1)
+                    _, score_2          = self.max_move(grid, depth + 1)
                     grid.board[r][c]    = 4
-                    score_4             = self.max_move(grid, depth + 1)
+                    _, score_4          = self.max_move(grid, depth + 1)
                     grid.board[r][c]    = 0
 
                     score               = score_2 * (1.0 - RATE) + score_4 * RATE
-                    score               = score + current_score * (1 if self.use_sum else 0)
+                    score              += current_score * int(self.__use_invert_sum) * self.__weights[depth]
 
-                    best_score          = best_score + score
+                    best_score         += score
                     cnt                += 1
-                    
-        return best_score * (1 / cnt)
+
+        return -1, best_score // cnt
 
     def best_move(self, grid: Grid):
-        moves       = [
-            grid.move_down,
-            grid.move_left,
-            grid.move_right,
-            # grid.move_up,
-        ]
-        if self.four_dir:
-            moves.append(grid.move_up)
+        move_idx, _ = self.max_move(grid, 0)
         
-        move_idx    = -1
-        INF         = self.eval.INFINITY * (self.max_depth if self.use_sum else 1)
-
-        best_score  = Point([-INF], self.eval.num_eval)
-
-        for idx, move in enumerate(moves):
-            save_board = list()
-            grid.copy(grid.board, save_board)
-            if move():
-                score       = self.expecti_move(grid, 1)
-                grid.copy(save_board, grid.board)
-
-                if best_score < score:
-                    best_score  = score.copy()
-                    move_idx    = idx
-
-        if move_idx == -1:
-            grid.move_up()
+        if move_idx != -1:
+            grid.move(move_idx, True)
         else:
-            moves[move_idx]()
+            if grid.can_move_up():
+                grid.move_up()
--- algorithm/minimax_alpha_beta.py
+++ algorithm/minimax_alpha_beta.py
@@ -11,125 +11,98 @@ class MinimaxAlphaBeta(Algorithm):
         self.__weights          = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256]
         # self.__weights          = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-    def max_move(self, grid: Grid, depth, alpha: int, beta: int) -> int:
-        current_score   = self.eval.evaluate(grid, True)
+    def max_move(self, grid: Grid, depth: int, alpha: int, beta: int) -> tuple[int, int]:
+        """
+        Returns
+        -------
+            tuple of best_move and best_score
+        
+        """
+        current_score       = self.eval.evaluate(grid, True)
         
         if depth >= self.max_depth:
             if self.__use_invert_sum:
-                return current_score * self.__weights[depth]
-            return current_score
+                return -1, current_score * self.__weights[depth]
+            return -1, current_score
 
         if current_score == self.eval.GAME_OVER:
-            return current_score * (self.max_depth - depth + 1) 
+            return -1, current_score * (self.max_depth - depth + 1) 
 
-        moves           = [
-            grid.move_down,
-            grid.move_left,
-            grid.move_right,
-            # grid.move_up,
-        ]
-        
-        can_moves       = [
-            grid.can_move_down,
-            grid.can_move_left,
-            grid.can_move_right,
-            # grid.can_move_up
-        ]
+        dirs                = [1, 2, 3]
+        best_move           = -1
+        best_score          = -self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
 
-        best_score      = -self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
-
-        for idx, move in enumerate(moves):
-            save_board  = list()
-            if can_moves[idx]():
+        for direct in dirs:
+            save_board      = list()
+            if grid.can_move(direct):
                 grid.copy(grid.board, save_board)
-                move()
-                score       = self.min_move(grid, depth + 1, alpha, beta)
+                grid.move(direct)
+                _, score    = self.min_move(grid, depth + 1, alpha, beta)
                 grid.copy(save_board, grid.board)
 
                 score      += current_score * int(self.__use_invert_sum) * self.__weights[depth]
                 
                 if best_score < score:
-                    best_score = score
+                    best_score  = score
+                    best_move   = direct
 
                 if alpha < best_score:
-                    alpha = best_score
+                    alpha       = best_score
                 if beta <= alpha:
-                    return best_score
+                    return best_move, best_score
 
-        return best_score
+        return best_move, best_score
 
-    def min_move(self, grid: Grid, depth, alpha: int, beta: int) -> int:
-        current_score = self.eval.evaluate(grid, False)
+    def min_move(self, grid: Grid, depth: int, alpha: int, beta: int) -> tuple[int, int]:
+        """
+        Returns
+        -------
+            tuple of best_move and best_score
+        """
+        current_score       = self.eval.evaluate(grid, False)
 
         if depth >= self.max_depth:
             if self.__use_invert_sum:
-                return current_score * self.__weights[depth]
-            return current_score
+                return -1, current_score * self.__weights[depth]
+            return -1, current_score
         
         if current_score == self.eval.GAME_OVER:
-            return current_score * (self.max_depth - depth + 1)
+            return -1, current_score * (self.max_depth - depth + 1)
 
-        ROW, COLUMN = grid.ROW, grid.COLUMN
-        RATE        = grid.RANDOM_4_RATE
+        ROW, COLUMN         = grid.ROW, grid.COLUMN
+        RATE                = grid.RANDOM_4_RATE
 
-        best_score  = self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
+        best_score          = self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
 
         for r in range(ROW):
             for c in range(COLUMN):
                 if grid.board[r][c] == 0:
                     grid.board[r][c]    = 2
-                    score_2             = self.max_move(grid, depth + 1, alpha, beta)
+                    _, score_2          = self.max_move(grid, depth + 1, alpha, beta)
                     grid.board[r][c]    = 4
-                    score_4             = self.max_move(grid, depth + 1, alpha, beta)
+                    _, score_4          = self.max_move(grid, depth + 1, alpha, beta)
                     grid.board[r][c]    = 0
 
                     score               = score_2 * (1.0 - RATE) + score_4 * RATE
                     score              += current_score * int(self.__use_invert_sum) * self.__weights[depth]
 
-                    if score < best_score:
-                        best_score      = score
+                    if best_score > score:
+                        best_score = score
 
                     if beta > best_score:
                         beta            = best_score
                     if beta <= alpha:
-                        return best_score
-        return best_score
+                        return -1, best_score
+                    
+        return -1, best_score
 
     def best_move(self, grid: Grid):
-        moves       = [
-            grid.move_down,
-            grid.move_left,
-            grid.move_right,
-            # grid.move_up,
-        ]
-
-        can_moves   = [
-            grid.can_move_down,
-            grid.can_move_left,
-            grid.can_move_right,
-            # grid.can_move_up
-        ]
-
-        move_idx    = -1
-
-        best_score  = -self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
-
-        for idx, move in enumerate(moves):
-            save_board = list()
-            if can_moves[idx]():
-                grid.copy(grid.board, save_board)
-                move()
-                alpha       = -self.eval.INFINITY
-                beta        = self.eval.INFINITY
-                score       = self.min_move(grid, 1, alpha, beta)
-                grid.copy(save_board, grid.board)
-
-                if best_score < score:
-                    best_score  = score
-                    move_idx    = idx
+        alpha       = -self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
+        beta        = self.eval.INFINITY * (self.max_depth if self.__use_invert_sum else 1)
+        move_idx, _ = self.max_move(grid, 0, alpha, beta)
         
         if move_idx != -1:
-            moves[move_idx](True)
+            grid.move(move_idx, True)
         else:
             if grid.can_move_up():
                 grid.move_up()
