5bd616fad31d366d11243d555089183d79d6d445
Nguyen Ngoc Dung <84455292+DungNasSa10@users.noreply.github.com>
2021-12-15 00:09:04 +0700

    Update minimax_alpha_beta.py

--- minimax_alpha_beta.py
+++ minimax_alpha_beta.py
@@ -1,55 +1,91 @@
 from game.grid import Grid
 from .algorithm import Algorithm
-
+from typing import Tuple
 
 class MinimaxAlphaBeta(Algorithm):
 
     def __init__(self, max_depth) -> None:
         super().__init__(max_depth)
 
-    def max_move(self, grid: Grid, alpha: int, beta: int, depth: int):
+    def max_move(self, grid: Grid, alpha: int, beta: int, depth: int) -> Tuple[int, int]:
+        """
+        Visual and calculate next move for max player (move the board)
+        Parameters
+        ----------
+            grid: Grid
+                the current grid
+            depth: int
+                current depth
+            alpha: int
+                alpha value used for pruning
+            beta: int
+                beta value used for pruning
+
+        Returns
+        -------
+            tuple of best_move and best_score
+        """
         current_score = self.eval.evaluate(grid, True)
 
-        if depth > self.max_depth or grid.is_terminal(who='max'):
-            return current_score
+        if depth > self.max_depth or grid.is_terminal('max'):
+            return -1, current_score
 
-        moves = grid.get_children('max')
-        best_move, max_score = moves[0], - self.eval.INFINITY
-
-        for move in moves:
-            state = Grid(None)
-            Grid.copy(grid.board, state.board)
-            state.move(move)
-            pre_score = max_score
-            max_score = max(max_score, self.exp_move(state, alpha, beta, depth + 1))
-
-            if max_score > pre_score and depth == 1:
-                best_move = move
-            if max_score >= beta:
-                return max_score
-            alpha = max(alpha, max_score)
-        if depth == 1:
-            return best_move, max_score
-        return max_score
-
-    def exp_move(self, grid: Grid, alpha: int, beta: int, depth: int):
-        current_score = self.eval.evaluate(grid, True)
-        moves = grid.get_children('min')
-        min_score = self.eval.INFINITY
+        moves = [1, 2, 3]
+        best_move, max_score = -1, -self.eval.INFINITY
 
         for move in moves:
-            state = Grid(None)
-            Grid.copy(grid.board, state.board)
-            state.place_tile(move[0], move[1], move[2])
-            min_score = min(min_score, self.max_move(state, alpha, beta, depth + 1))
+            save_board = list()
+            if grid.can_move(move):
+                grid.copy(grid.board, save_board)
+                grid.move(move)
+                _, score = self.min_move(grid, alpha, beta, depth + 1)
+                grid.copy(save_board, grid.board)
+                if score > max_score:
+                    best_move, max_score = move, score
 
-            if min_score <= alpha:
-                return min_score
-            beta = min(beta, min_score)
-        if depth == 1:
-            return None, min_score
+        return best_move, max_score
 
-        return min_score
+    def min_move(self, grid: Grid, alpha: int, beta: int, depth: int):
+        """
+        Visual and calculate next move for expect player (random new tile)
+        Parameters
+        ----------
+            grid: Grid
+                the current grid
+            depth: int
+                current depth
+            alpha: int
+                alpha value used for pruning
+            beta: int
+                beta value used for pruning
+
+        Returns
+        -------
+            tuple of best_move and best_score
+        """
+        current_score = self.eval.evaluate(grid, False)
+
+        if depth > self.max_depth or grid.is_terminal('min'):
+            return -1, current_score
+
+        ROW, COLUMN = grid.ROW, grid.COLUMN
+        RATE        = grid.RANDOM_4_RATE
+        min_score   = self.eval.INFINITY
+
+        for r in range(ROW):
+            for c in range(COLUMN):
+                if grid.board[r][c] == 0:
+                    grid.board[r][c] = 2
+                    _, score_2 = self.max_move(grid, alpha, beta, depth + 1)
+                    grid.board[r][c] = 4
+                    _, score_4 = self.max_move(grid, alpha, beta, depth + 1)
+                    grid.board[r][c] = 0
+                    
+                    score = min(score_2, score_4)
+                    min_score = min(min_score, score)
+
+        return -1, min_score
+        # there is no need to find best move for min!!!
 
     def best_move(self, grid: Grid):
         best_move, max_score = self.max_move(grid, - self.eval.INFINITY, self.eval.INFINITY, 1)
