1f83f799ecbbf14a53cae74289721d89bb2938e4
Nguyen Ngoc Dung <84455292+DungNasSa10@users.noreply.github.com>
2021-12-15 15:18:27 +0700

    Update minimax_alpha_beta.py

--- minimax_alpha_beta.py
+++ minimax_alpha_beta.py
@@ -29,7 +29,7 @@ class MinimaxAlphaBeta(Algorithm):
         if depth > self.max_depth or grid.is_terminal('max'):
             return -1, current_score
 
-        moves = [1, 2, 3]
+        moves = [0, 1, 2, 3]
         best_move, max_score = -1, -self.eval.INFINITY
 
         for move in moves:
@@ -37,14 +37,19 @@ class MinimaxAlphaBeta(Algorithm):
             if grid.can_move(move):
                 grid.copy(grid.board, save_board)
                 grid.move(move)
-                _, score = self.min_move(grid, alpha, beta, depth + 1)
+                pre_score = max_score
+                max_score = max(max_score, self.min_move(grid, alpha, beta, depth + 1)[1])
                 grid.copy(save_board, grid.board)
-                if score > max_score:
-                    best_move, max_score = move, score
+
+                if max_score > pre_score and depth == 1:
+                    best_move = move
+                if max_score > beta:
+                    return best_move, max_score
+                alpha = max(alpha, max_score)
 
         return best_move, max_score
 
-    def min_move(self, grid: Grid, alpha: int, beta: int, depth: int) -> tuple[int, int]:
+    def min_move(self, grid: Grid, alpha, beta, depth: int) -> tuple[int, int]:
         """
         Visual and calculate next move for expect player (random new tile)
         Parameters
@@ -68,7 +73,6 @@ class MinimaxAlphaBeta(Algorithm):
             return -1, current_score
 
         ROW, COLUMN = grid.ROW, grid.COLUMN
-        RATE        = grid.RANDOM_4_RATE
         min_score   = self.eval.INFINITY
 
         for r in range(ROW):
@@ -79,9 +83,11 @@ class MinimaxAlphaBeta(Algorithm):
                     grid.board[r][c] = 4
                     _, score_4 = self.max_move(grid, alpha, beta, depth + 1)
                     grid.board[r][c] = 0
-                    
-                    score = min(score_2, score_4)
-                    min_score = min(min_score, score)
+                    min_score = min(min_score, score_2, score_4)
+
+                    if min_score < alpha:
+                        return -1, min_score
+                    beta = min(beta, min_score)
 
         return -1, min_score
         # there is no need to find best move for min!!!
@@ -91,6 +97,6 @@ class MinimaxAlphaBeta(Algorithm):
 
         if move_direction != -1:
             grid.move(move_direction, True)
-        else:
-            if grid.can_move_up():
-                grid.move_up(True)
+        #else:
+            #if grid.can_move_up():
+                #grid.move_up(True)
